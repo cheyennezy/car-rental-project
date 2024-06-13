c22f3bb6a428a462e3bff270e74e2ef780edcba4
yyyyyyyyyy9 <tomorrow700888@gmail.com>
2024-06-13 19:34:10 +0800

    Update returncar.php

--- returncar.php
+++ returncar.php
@@ -45,7 +45,6 @@ $conn = Connect();
             <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Control Panel <span class="caret"></span> </a>
                 <ul class="dropdown-menu">
               <li> <a href="entercar.php">Add Car</a></li>
-              <li> <a href="enterdriver.php"> Add Driver</a></li>
               <li> <a href="clientview.php">View</a></li>
 
             </ul>
@@ -119,19 +118,16 @@ function dateDiff($start, $end) {
     return round($diff / 86400);
 }
  $id = $_GET["id"];
- $sql1 = "SELECT c.car_name, c.car_nameplate, rc.rent_start_date, rc.rent_end_date, rc.fare, rc.charge_type, d.driver_name, d.driver_phone
- FROM rentedcars rc, cars c, driver d
- WHERE id = '$id' AND c.car_id=rc.car_id AND d.driver_id = rc.driver_id";
+ $sql1 = "SELECT c.car_name, c.car_nameplate, rc.rent_start_date, rc.rent_end_date, rc.charge_type, 
+ FROM rentedcars rc, cars c
+ WHERE id = '$id' AND c.car_id=rc.car_id ;
  $result1 = $conn->query($sql1);
  if (mysqli_num_rows($result1) > 0) {
     while($row = mysqli_fetch_assoc($result1)) {
         $car_name = $row["car_name"];
         $car_nameplate = $row["car_nameplate"];
-        $driver_name = $row["driver_name"];
-        $driver_phone = $row["driver_phone"];
         $rent_start_date = $row["rent_start_date"];
         $rent_end_date = $row["rent_end_date"];
-        $fare = $row["fare"];
         $charge_type = $row["charge_type"];
         $no_of_days = dateDiff("$rent_start_date", "$rent_end_date");
     }
@@ -143,8 +139,7 @@ function dateDiff($start, $end) {
         <form role="form" action="printbill.php?id=<?php echo $id ?>" method="POST">
         <br style="clear: both">
           <h3 style="margin-bottom: 5px; text-align: center; font-size: 30px;"> Journey Details </h3>
-          <h6 style="margin-bottom: 25px; text-align: center; font-size: 12px;"> Allow your driver to fill the below form </h6>
-
+          
            <h5> Car:&nbsp;  <?php echo($car_name);?></h5>
 
            <h5> Vehicle Number:&nbsp;  <?php echo($car_nameplate);?></h5>
@@ -153,18 +148,8 @@ function dateDiff($start, $end) {
 
            <h5> End Date:&nbsp;  <?php echo($rent_end_date);?></h5>
 
-           <h5> Fare:&nbsp;  Rs. <?php 
-            if($charge_type == "days"){
-                    echo ($fare . "/day");
-                } else {
-                    echo ($fare . "/km");
-                }
-            ?>
-            </h5>
-
-           <h5> Driver Name:&nbsp;  <?php echo($driver_name);?></h5>
-
-           <h5> Driver Contact:&nbsp;  <?php echo($driver_phone);?></h5>
+           
+    
           <?php if($charge_type == "km") { ?>
           <div class="form-group">
             <input type="text" class="form-control" id="distance_or_days" name="distance_or_days" placeholder="Enter the distance travelled (in km)" required="" autofocus>
